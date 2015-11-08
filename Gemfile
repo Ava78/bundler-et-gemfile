4c41c8ed3d868080d6bb141fb3d9356e28675a73
riquet <eric.merlin3@gmail.com>
2015-11-08 22:30:30 +0100

    Mise en place d'une variable pour afficher la date et l'heure de chaque qrcode automatiquement

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 # Version Ruby
-ruby '2.2.1'
+ruby '2.2.0'
 
 # RQRcode_png
 gem 'rqrcode_png', '~> 0.1.5'
--- qr_code.rb
+++ qr_code.rb
@@ -1,14 +1,15 @@
 require 'rqrcode_png'
 require 'pry'
+require 'date'
 
 puts 'Au p\'tit bijour, veuillez saisir vot tixt si you plé:'
 reponse = gets.chomp
 
 qr = RQRCode::QRCode.new( reponse, :size => 4, :level => :h )
-
+ma_date = Time.now
 # Enregistre le QRcode
 png = qr.to_img
-png.resize(120, 120).save("reponse.png")
+png.resize(120, 120).save("#{ma_date.strftime(('Le %d-%m-%Y à %H:%M'))}.png")
 
 # Affiche dans le terminal
 qr.modules.each do |row|
